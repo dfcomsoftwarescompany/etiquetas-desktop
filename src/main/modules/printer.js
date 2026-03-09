88215ec1491a9ca780dad369803bad74aa85288c
ivan-lucass <ivanlucas.dev@gmail.com>
2026-03-09 17:31:27 -0300

    fix: adjust layot

--- src/main/modules/printer.js
+++ src/main/modules/printer.js
@@ -579,7 +579,7 @@ class PrinterManager {
   }
 
   async printCoupons(printerName, coupons) {
-    const canvas = createCanvas(1600, 2400);
+    const canvas = createCanvas(1300, 2000);
     const ctx = canvas.getContext('2d');
 
     for (const coupon of coupons) {
